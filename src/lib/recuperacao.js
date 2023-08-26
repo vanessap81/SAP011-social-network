51f4babc8793ee64ad5fe7fe9ce6b1e46be7cc3d
jumvribeiro <juliamvribeiro@gmail.com>
2023-08-26 13:50:38 -0300

    tela recuperacaosenha

---
+++ src/lib/recuperacao.js
@@ -0,0 +1,45 @@
+export default () => {
+const content = `
+                <h1>Recuperação de Senha</h1>
+                <p>Confirme seu e-mail</p>
+
+                <div id="form-container">
+                <form action="">
+                <label for="email">E-mail</label>
+                <p><input class="input" type="text" id="email" placeholder="seunome@email.com" required name="email" /></p>
+
+                <div class="espaço-redefinir">
+                <button type="submit" class="btn-redefinir-senha" id="btn-redefinir-senha">Enviar</button>
+                </div>
+
+                <p>Você receberá um e-mail com o link para cadastrar uma nova senha.</p>
+
+                </form>
+                <p id="msg-redefine-senha" class="msg-redefine-senha"></p>
+
+                <p>by Julia Ribeiro e Vanessa Ponte</p>
+
+                </div>
+                `;
+                
+container.innerHTML = content;
+
+        const botaoRedefinir = container.querySelector("#btn-redefinir-senha");
+        botaoRedefinir.addEventListener('click', (e) => {
+                e.preventDefault();
+                const email = container.querySelector('#email').value;
+                redefinirSenha(email)
+                .then(() => {
+                const msgConfirmaRedefinicao = container.querySelector("#msg-redefine-senha");
+                msgConfirmaRedefinicao.innerHTML = "Em breve, você receberá um e-mail para redefinir sua senha.";
+                })
+                .catch((error) => {
+                const msgErroRedefinicao = container.querySelector("#msg-redefine-senha");
+                if (error.code === "auth/user-not-found") {
+                msgErroRedefinicao.innerHTML = "O e-mail informado não está cadastrado.";
+                }
+                });
+        });
+              
+        return container;
+};
--- src/main.js
+++ src/main.js
@@ -1,28 +1,12 @@
 // Este es el punto de entrada de tu aplicacion
 
+import recuperacao from "./lib/recuperacao";
+
 // import { myFunction } from './lib/index.js';
 
-// myFunction();
-
-function getContent(fragmentId) {
-  const pages = {
-    home: "This is the Home page. Welcome to my site.",
-    about: "This page will describe what my site is about",
-    contact: "Contact me on this page if you have any questions",
-  };
-  return pages[fragmentId];
+function fillMain() {
+  const mainContent = document.getElementById("app");
+  mainContent.innerHTML = recuperacao;
 }
 
-function loadContent() {
-  const contentDiv = document.getElementById("app");
-  const fragmentId = location.hash.substring(1);
-  contentDiv.innerHTML = getContent(fragmentId);
-}
-
-if (!location.hash) {
-  location.hash = "#home";
-}
-
-loadContent();
-
-window.addEventListener("hashchange", loadContent);
+fillMain();
